acb929bf955efebdeb025d154ef5cf3c846b7016
Your Github User Name <ramirezr@protonmail.com>
2022-03-10 15:29:46 -0600

    added notes

--- calculator.js
+++ calculator.js
@@ -3,10 +3,13 @@
 const express = require('express');
 const app = express();
 
+//setting up home route
 app.get("/", function(request, response){
     response.send("<h1>Hello World</h1");
 });
 
+
+//sets up server
 app.listen(3000, function() {
     console.log("Server started on port 3000");
 });
